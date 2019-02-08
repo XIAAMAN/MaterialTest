97acbfc60429b5d52219a2abea2b5de07f05e9d5
xiaaman <756585746@qq.com>
2019-02-08 13:13:55 +0800

    Three Commit

--- app/src/main/java/com/example/materialtest/FruitAdapter.java
+++ app/src/main/java/com/example/materialtest/FruitAdapter.java
@@ -1,6 +1,59 @@
 package com.example.materialtest;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class FruitAdapter extends RecyclerView {
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder>{
+    private Context mContext;
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView fruitImage;
+        TextView fruitName;
+        public ViewHolder(View view) {
+            super(view);
+            cardView = (CardView) view;
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+    //创建ViewHolder实例，并把加载进来的布局传入到构造函数当中，最后将ViewHolder的实例返回
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mContext == null) {
+            mContext = parent.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    //对子项数据进行赋值
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitName.setText((fruit.getName()));
+        Glide.with(mContext).load(fruit.getImageId()).into(holder.fruitImage);
+    }
+
+    //一共有多少子项
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
 }
